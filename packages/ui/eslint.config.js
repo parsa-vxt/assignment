419cc65d6d243399394b531d39f36b534a602be7
Parsa VxT <57572461+parsa-vxt@users.noreply.github.com>
2025-12-12 08:16:01 +0000

    Fix Storybook's dev server

--- packages/ui/eslint.config.js
+++ packages/ui/eslint.config.js
@@ -1,6 +1,4 @@
-// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
-import storybook from "eslint-plugin-storybook";
-
+/* eslint-disable import/no-extraneous-dependencies */
 import { defineConfig } from '@fullstacksjs/eslint-config';
 import globals from 'globals';
 
--- packages/ui/src/Button/Button.tsx
+++ packages/ui/src/Button/Button.tsx
@@ -1,5 +1,5 @@
 import type { ComponentProps } from 'react';
 
 export const Button = (props: ComponentProps<'button'>) => (
-   <button {...props} />
+   <button {...props}  className='bg-green-400'/>
 );
